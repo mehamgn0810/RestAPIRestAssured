4db5476edef1d0f877cd31f2edb5c8351534e389
Mehavarnan <meha10aug@gmail.com>
2018-08-14 11:27:19 +0530

    Added Serialization and DeSerialization Test

---
+++ src/gettingstarted/Rectangle.java
@@ -0,0 +1,27 @@
+package gettingstarted;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Rectangle implements Serializable{
+
+	private double height;
+	private double width;
+ 
+	public Rectangle(double height, double width)
+	{
+		this.height = height;
+		this.width = width;
+	}
+ 
+	public double Area()
+	{
+		return height * width;
+	}
+ 
+	public double Perimeter()
+	{
+		return 2 * (height + width);
+	}
+	
+}
