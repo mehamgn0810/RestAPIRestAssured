f8b8c348aca3919161881b5b8788ca026d6e437f
Mehavarnan.m <Mehavarnan.m@CHNM90P28.thirdware.com>
2018-12-05 18:50:46 +0530

    update

--- src/gettingstarted/ValidatingJSONResponseBody.java
+++ src/gettingstarted/ValidatingJSONResponseBody.java
@@ -1,16 +1,27 @@
 package gettingstarted;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
 
 public class ValidatingJSONResponseBody {
 
 	public static void main(String[] args) {
 		
-		RestAssured.baseURI = "https://api.itbook.store";
+		
+		RestAssured.baseURI = "https://www.metaweather.com";
 		
 		given().
-			param("key", "qaclick123");
+			param("query", "san").
+			
+		when().
+			get("/api/location/search").
+		
+		then().
+			assertThat().statusCode(200).and().
+			contentType(ContentType.JSON).and().
+			body("woeid", equalTo("2487796"));
 
 	}
 
